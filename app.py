d6e1977884f4b0c71af28ff15ab39d51187d62d3
SALVADOR VALVERDE BASTIDAS <91941056+Rodavlas80@users.noreply.github.com>
2022-06-11 09:17:31 +0200

    Added KPI with Twitch data

--- app.py
+++ app.py
@@ -73,7 +73,15 @@ app.layout = html.Div([
                 id='twitter-visit',
             )
         ]),
-    ], style={"columnCount": 4, 'textAlign': "center"}),
+        html.Div([
+            html.Img(src="https://upload.wikimedia.org/wikipedia/commons/d/d3/Twitch_Glitch_Logo_Purple.svg",
+                     style={"width": "100px"}),
+            html.H2(
+                id='twitch-visit',
+            )
+        ]),
+        
+    ], style={"columnCount": 5, 'textAlign': "center"}),
     html.H3('Total Visits by Month', style={"textAlign": "center"}),
     dcc.Graph(
         id='total-visit-line'
@@ -100,6 +108,7 @@ app.layout = html.Div([
     Output('facebook-visit', 'children'),
     Output('instagram-visit', 'children'),
     Output('twitter-visit', 'children'),
+    Output('twitch-visit', 'children'),
     Output('total-visit-line', 'figure'),
     Output('total-visit-social-networks-line', 'figure'),
     Output('world-map', 'figure'),
@@ -144,6 +153,15 @@ def update_figures(start_date_selected, end_date_selected, social_networks_selec
              (df.datetime >= start_date_selected) &
              (df.datetime <= end_date_selected)]
     ).shape[0]
+    
+    twitch_visit = (
+        df
+        .loc[(df.social_network == 'twitch') &
+             (df.social_network.isin(social_networks_selected)) &
+             (df.device.isin(devices_selected)) &
+             (df.datetime >= start_date_selected) &
+             (df.datetime <= end_date_selected)]
+    ).shape[0]    
 
     df_by_month = (
         df
@@ -238,7 +256,7 @@ def update_figures(start_date_selected, end_date_selected, social_networks_selec
         }
     )
 
-    return total_visit, facebook_visit, instagram_visit, twitter_visit, total_visit_fig, total_visit_social_network_fig, world_map_fig, devices_pie_fig
+    return total_visit, facebook_visit, instagram_visit, twitter_visit, twitch_visit, total_visit_fig, total_visit_social_network_fig, world_map_fig, devices_pie_fig
 
 
 if __name__ == '__main__':
